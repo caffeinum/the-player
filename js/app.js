bf7bcd6e9acbd3b2def49526ad90639c556e1d16
Alex Bykhun <theisease@gmail.com>
2015-02-14 17:57:36 +0300

    this.current

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ window.onload = function (artwork1, artwork2) {
 	
 	mixer = new Mixer();
 	rec = new Recommender();
-	billy = new Track('/music/track1.mp3', null);
+	billy = new Track('/music/track2.mp3', null);
 	
 	mixer.load( billy );
 	
--- js/mixer.js
+++ js/mixer.js
@@ -2,58 +2,76 @@ var Mixer = function () {
     var context = new webkitAudioContext();
 
     // constructor
-	this.current = null;
-	this.next = null;
+	this.current = this.next = {
+		gain: null,
+		request: null,
+		source: null
+	};
     
 	var request, source;
-    var gain_current  = context.createGain(), 
-        gain_next = context.createGain();
-    gain_current.connect( context.destination );
-    gain_next.connect( context.destination );
+    this.current.gain	= context.createGain(), 
+    this.next.gain		= context.createGain();
+    this.current.gain.connect( context.destination );
+    this.next	.gain.connect( context.destination );
 	var audio = null; // anything...
 	
 	this.load = function ( track ) {
-        source = context.createBufferSource();    
+        this.current.source = context.createBufferSource();    
         // use $.get !!
-        request = new XMLHttpRequest();
+        this.current.request = new XMLHttpRequest();
         
-        request.open('GET', track.url, true); 
-        request.responseType = 'arraybuffer';
-		request.onload = this.play;
-        request.send();
+        this.current.request.open('GET', track.url, true); 
+        this.current.request.responseType = 'arraybuffer';
+        this.current.request.send();
+		
+		this.current.request.onload = this.play;
 	};
 	
 	this.play = function () {
-		console.log( request );
-        context.decodeAudioData(request.response, function(response) {
-            source.buffer = response;
+		if ( ! this.current ) return;
+        context.decodeAudioData(this.current.request.response, function( response ) {
+            this.current.source.buffer = response;
             //beatFind(source.buffer);
-			console.log( source );
-            this.current = source;
+			
+            this.current.source = source;            
+            this.current.source.connect( gain_current );
             
-            this.current.connect( gain_current );
-            
-            this.current.start(0);
+            this.current.source.start(0);
         }, function () { console.error('The request failed.'); } );
     };
     
 	this.pause = function () {};
 	this.prepare = function ( track ) {
 		// load track into the deck2
-        this.next = context.createBufferSource();
+        this.next.source = context.createBufferSource();
         //buffer? cache?
-        this.next.buffer = ;
-        this.next.connect( gain_next );
+		
+		this.next.request = new XMLHttpRequest();
+        
+        this.next.request.open('GET', track.url, true); 
+        this.next.request.responseType = 'arraybuffer';
+        this.next.request.send();
+		
+        this.next.source.buffer = null;
+        this.next.source.connect( gain_next );
     };
 	
 	this.mix = function () {
 		// skips to the deck2
-        this.next.start(0);
-        gain_next.gain = 0;
-        setInterval(function(){
-            gain_current.gain -= 0.002;
-            gain_next.gain += 0.002;
-	}, 10)
-    this.current.stop(0);
+        //this.next.start(0);
+        //gain_next.gain = 0;
+        var int = setInterval(function(){
+            this.current.gain.gain.value -= 0.002;
+            this.next.gain.gain.value += 0.002;
+			
+			if ( this.current.gain.gain.value <= 0 ) {
+    			this.current.source.stop(0);
+				
+				// interchange
+				clearInterval( int );
+			}	
+		}, 10);
+		
+	};
     //change places;
 };
