ee976747a73624fbcecab492bd4d080f810f5877
Alex Bykhun <theisease@gmail.com>
2015-02-15 01:34:43 +0300

    Getting code param

--- js/auth.js
+++ js/auth.js
@@ -1,24 +1,26 @@
 var Auth = function () {
+		var code;
+		this.code = code;
 	
+		// save cookie
 	
 		this.getAccessToken = function ( handler ) {
             var token = '';
-			/*jQuery.getJSON('https://oauth.vk.com/access_token?' + 
+			
+			if ( this.code ) {
+				return handler( this.code );
+			}/*
+			jQuery.getJSON('https://oauth.vk.com/access_token?' + 
 						  'client_id=' + config.app_id +
 						  '&client_secret=' + config.secret + 
-						  '&v=5.1&grant_type=client_credentials',
+						  '&v=5.28',
 						  handler);*/
             //
             var url = "https://oauth.vk.com/authorize?client_id=" + config.app_id + 
-                "&scope=friends,audio&redirect_uri=/redirect.html&display=popup&v=5.28&response_type=code";
-            var newWin = window.open(url, "AuthForm", "width=200");
-            newWin.onload = function () {
-                console.log( newWin );
-            }
-            token = '';
-            
-            handler( token );
-            //jQuery.getJSON('https://oauth.vk.com/blank.html', handler);
-
+				"&scope=friends,audio" +
+				"&redirect_uri=" +
+				"http://127.0.0.1:49866/test/redirect.html" +
+				"&display=popup&v=5.28&response_type=code";
+            document.location = encodeURI( url );
 		};
 };
