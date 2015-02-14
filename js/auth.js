6cd4e1d7406c05db904a5f2b91c0f071e6558144
Vasiliy Kerimov <mostlybrainless@gmail.com>
2015-02-14 23:54:30 +0300

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,17 @@
 var Auth = function () {
 	
-	VK = {
-		getAccessToken: function ( handler ) {
+	
+		this.getAccessToken = function ( handler ) {
 			jQuery.getJSON('https://oauth.vk.com/access_token?' + 
 						  'client_id=' + config.app_id +
 						  '&client_secret=' + config.secret + 
 						  '&v=5.1&grant_type=client_credentials',
 						  handler);
-		}
-	};
+            //https://oauth.vk.com/authorize?client_id=" + config.app_id + "&scope=PERMISSIONS&redirect_uri=REDIRECT_URI&display=DISPLAY&v=API_VERSION&response_type=token
+            var newWin = window.open("http:fantlab.ru", "AuthForm", "");
+            newWin.focus();
+            
+
+
+		};
 };
