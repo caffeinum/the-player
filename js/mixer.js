c9d79e107ef43e8f6ae550154ece63753e63e2f6
SchattenGenie <SchattenGenie@gmail.com>
2015-02-14 17:20:34 +0300

    no pain no gain

--- js/mixer.js
+++ js/mixer.js
@@ -6,6 +6,10 @@ var Mixer = function () {
 	this.next = null;
     
 	var request, source;
+    var gain_current  = context.createGain(), 
+        gain_next = context.createGain();
+    gain_current.connect( context.destination );
+    gain_next.connect( context.destination );
 	var audio = null; // anything...
 	
 	this.load = function ( track ) {
@@ -25,20 +29,31 @@ var Mixer = function () {
             source.buffer = response;
             //beatFind(source.buffer);
 			console.log( source );
+            this.current = source;
             
-            source.start(0);
-			source.connect( context.destination );
+            this.current.connect( gain_current );
+            
+            this.current.start(0);
         }, function () { console.error('The request failed.'); } );
     };
     
 	this.pause = function () {};
 	this.prepare = function ( track ) {
 		// load track into the deck2
-
+        this.next = context.createBufferSource();
+        //buffer? cache?
+        this.next.buffer = ;
+        this.next.connect( gain_next );
     };
 	
 	this.mix = function () {
 		// skips to the deck2
-        
-	};
+        this.next.start(0);
+        gain_next.gain = 0;
+        setInterval(function(){
+            gain_current.gain -= 0.002;
+            gain_next.gain += 0.002;
+	}, 10)
+    this.current.stop(0);
+    //change places;
 };
