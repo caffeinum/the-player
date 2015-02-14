84735de64365fa5f0ab7f041c948a15d0e61a6c9
SchattenGenie <SchattenGenie@gmail.com>
2015-02-15 02:22:16 +0300

    Some BPM

--- js/mixer.js
+++ js/mixer.js
@@ -33,6 +33,66 @@ var Mixer = function () {
 	var current	= new Deck();
 	var next	= new Deck();
 	
+    function low_rendering(buffer, handler){
+        var OfflineContext = new webkitOfflineAudioContext(1, buffer.length, buffer.sampleRate);
+        var source = OfflineContext.createBufferSource();
+        source.buffer = buffer;
+          
+        var lowpass = OfflineContext.createBiquadFilter();
+        lowpass.type = "lowpass";
+        source.connect(lowpass);
+        lowpass.connect(OfflineContext.destination);
+        OfflineContext.startRendering();
+        source.start(0);
+        console.log( source );
+        OfflineContext.oncomplete = function(e) {
+            var filteredBuffer = e.renderedBuffer;    
+            handler(filteredBuffer);
+        }; 
+  }
+    
+    function beatFind(FilteredBuffer){
+        buffer = FilteredBuffer.getChannelData(0);
+        var peaksArray = [];
+        var length = buffer.length;
+        threshold = 1;
+        do{
+            for(var i = 0; i < length; i++) {
+                if (buffer[i]*buffer[i] > threshold) {
+                    peaksArray.push((i+5000)/44.1);
+                    i += 10000;
+                }
+                i++;
+            }
+            threshold -= 0.05;
+        } while(peaksArray.length < 140)
+            console.log(peaksArray);
+        var intervalCounts = [];
+        peaksArray.forEach(function(peak, index) {
+            for(var i = 0; i < 10; i++) {
+                var interval = peaksArray[index + i] - peak;
+                var foundInterval = intervalCounts.some(function(intervalCount) {
+                    if (intervalCount.interval === interval)
+                        return intervalCount.count++;
+                });
+                if (!foundInterval) {
+                    intervalCounts.push({
+                        interval: interval,
+                        count: 1
+                    });
+                }
+            }
+        });
+      
+        intervalCounts.sort(function(a,b){return b.count - a.count});
+        console.log(intervalCounts);
+        for(var i = 0; i < intervalCounts.length; i++)
+            if(intervalCounts[i].interval >= 1500 && intervalCounts[i].interval <= 5000) break;
+        var BPM = (4*60000/intervalCounts[i].interval);
+        return BPM;
+}
+    
+    
 	this.playing = false;
 	
 	this.load = function ( track ) {
@@ -74,6 +134,9 @@ var Mixer = function () {
 		next.gain.gain.value = 0;
 		
 		var int = setInterval(function(){
+         //   low_rendering(source_s.buffer, function (filteredBuffer) {
+       //         var BPM = beatFind(filteredBuffer);
+            
             current.gain.gain.value -= 0.002;
             next.gain.gain.value += 0.002;
 			
